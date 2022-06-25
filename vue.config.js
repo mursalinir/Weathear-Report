9f9074b51f23bd278252c92a15843f754fa27006
Mursalin Islam <mursalinir@gmail.com>
2022-06-26 00:21:14 +0600

    change config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-}), {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Weather-Report/'
-    : '/'  
-}
+module.exports = {
+  publicPath: "/vue-weather-app/",
+};
